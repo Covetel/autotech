eeb1350e0c1fbb0709efae984b4882887e12f5fe
Carlos Paredes <cparedes@covetel.com.ve>
2014-06-09 11:51:15 -0430

    Inicializando el módulo Autotech Product
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

---
+++ autotech_product/__init__.py
@@ -0,0 +1,1 @@
+import autotech_product
---
+++ autotech_product/__openerp__.py
@@ -0,0 +1,13 @@
+{
+  "name": "Autotech Product",
+  "version": "1.0",
+  "depends": ["base"],
+  "author": "Exds Inc.",
+  "category": "Products",
+  "description": "Product module for Autotech",
+  "data": [
+      "autotech_product_view.xml",
+  ],
+  "installable": True,
+  "auto_install": False,
+}
