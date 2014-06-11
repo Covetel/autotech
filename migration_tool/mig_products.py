e47cd46ff33bd3dd32d984dafd91ea74e32a4896
Carlos Paredes <cparedes@covetel.com.ve>
2014-06-11 02:25:41 -0430

    Avance en script de migración de productos #31
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- migration_tool/mig_products.py
+++ migration_tool/mig_products.py
@@ -1,41 +1,112 @@
+# *-* coding=utf-8 *-*
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-Base = automap_base()
- 
-#Conexion
-engine = create_engine('mysql://root:123321...@localhost/autotech')
+import xmlrpclib, pprint
 
-Base.prepare(engine, reflect=True)
+mysql_dbuser = 'root'
+mysql_dbpass = '123321...'
+mysql_dbname = 'autotech'
+mysql_dbhost = 'localhost'
 
-#Schemas de la tablas
-products = Base.classes.products
-products_description = Base.classes.products_description
+odoo_dbuser = 'admin'
+odoo_dbpass = '123321...'
+odoo_dbname = 'autotech'
+odoo_dbhost = 'localhost'
 
-#De aqui saco el id para la categoria
-products_attributes =  Base.classes.products_attributes
+def get_product_categ_id(sock, uid, dbname, dbpass, product_categ_name):
 
-#Lo que es en odoo category type
-products_options = Base.classes.products_options
+    filt = [('name','=', product_categ_name)]
 
-session = Session(engine)
+    categ_id = sock.execute(dbname, uid, dbpass, 'product.public.category', 'search', filt)
 
-#Query de productos all o first
-products = session.query(products).all()
+    return categ_id
 
-#Query atributos productos
-for product in products: 
-    query_attributes = session.query(products_attributes).filter(products_attributes.products_id == product.products_id)
-    attributes = query_attributes.all()
+def create_autotech_products(dbuser, dbpass, dbname, dbhost, product):
+    # Get the uid
+    sock_common = xmlrpclib.ServerProxy ('http://'+dbhost+':8069/xmlrpc/common')
+    uid = sock_common.login(dbname, dbuser, dbpass)
 
-    for attr in attributes:
-        try:
-            query_categories = session.query(products_options).filter(products_options.products_options_id == attr.options_id)
-            product_types = query_categories.all()
+    sock = xmlrpclib.ServerProxy('http://'+dbhost+':8069/xmlrpc/object')
 
-            for typ in product_types:
-                print typ.products_options_name
+    try:
+        categ_name = str(product['public_categ_id']).title()
+    except KeyError:
+        pass
 
-        except AttributeError:
-            print "product_id no encontrado en la tabla products_attributes"
+    try:
+        if categ_name:
+            categ_id = get_product_categ_id(sock, uid, dbname, dbpass, categ_name)
+    except UnboundLocalError:
+        pass
+
+    try:
+        if categ_id:
+            product.update({'public_categ_id': categ_id[0]})
+        else:
+            try:
+                del product['public_categ_id']
+            except KeyError:
+                pass
+    except UnboundLocalError:
+        pass
+
+    product_id = sock.execute(dbname, uid, dbpass, 'product.product', 'create', product)
+    print "Creado producto %s" % str(product['name'])
+
+def build_dict_products(dbuser, dbpass, dbhost, dbname):
+    product_list = []
+    autotech_product = {}
+
+    Base = automap_base()
+     
+    engine = create_engine('mysql://'+dbuser+':'+dbpass+'@'+dbhost+'/'+dbname+'')
+
+    Base.prepare(engine, reflect=True)
+
+    #Table schemes
+    products = Base.classes.products
+    products_description = Base.classes.products_description
+
+    #Table with id for categories
+    products_attributes =  Base.classes.products_attributes
+
+    #Table with types of products
+    products_options = Base.classes.products_options
+
+    session = Session(engine)
+
+    #Products query
+    products = session.query(products).all()
+    i = 0
+
+    #Products Attributes Query
+    for product in products: 
+        autotech_product.update({'name' : product.products_model})
+
+        query_descriptions = session.query(products_description).filter(products_description.products_id == product.products_id)
+        descriptions = query_descriptions.all()
+
+        for description in descriptions:
+            autotech_product.update({'more_info' : description.products_description})
+
+        query_attributes = session.query(products_attributes).filter(products_attributes.products_id == product.products_id)
+        attributes = query_attributes.all()
+
+        for attr in attributes:
+            try:
+                query_categories = session.query(products_options).filter(products_options.products_options_id == attr.options_id)
+                product_types = query_categories.all()
+
+                for typ in product_types:
+                    autotech_product.update({'public_categ_id' : typ.products_options_name})
+            except AttributeError:
+                print "product_id no encontrado en la tabla products_attributes"
+
+        create_autotech_products(odoo_dbuser, odoo_dbpass, odoo_dbname, odoo_dbhost, autotech_product)
+
+def main():
+    build_dict_products(mysql_dbuser, mysql_dbpass, mysql_dbhost, mysql_dbname)
+
+main()
